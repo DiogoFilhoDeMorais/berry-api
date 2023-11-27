669cfa390510172158ae56507319a5d9f9d547fb
SilwiuEduard <silviu.chiriloaie.it@gmail.com>
2023-11-27 16:57:38 +0200

    added CORS via package

--- server3.js
+++
@@ -1,9 +0,0 @@
-import http from "http";
-import app from "./index.js";
-
-const port = process.env.PORT || 3000;
-
-app.set("port", port);
-const server = http.createServer(app);
-
-server.listen(port);
